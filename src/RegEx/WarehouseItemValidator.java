bd8baa59436f5113e211f6d8e1ed7234c6e2b70b
bhild <bryanhildebrand1@gmail.com>
2021-01-26 18:08:13 -0800

    added shell regex

--- src/RegEx/WarehouseItemValidator.java
+++ src/RegEx/WarehouseItemValidator.java
@@ -5,13 +5,13 @@ public class WarehouseItemValidator {
 	//left empty -- this class will only be used as a part of the item list
 	//it is going to keep separate for organizational reasons
 	public boolean isValidId(String id) {
-		return false;
+		return id.matches("");
 	}
 	public boolean isValidName(String name) {
-		return false;
+		return name.matches("");
 	}
 	public boolean isValidDescription(String des) {
-		return false;
+		return des.matches("^(([:alpha:]|[, ]){5,}[.!;?])+$");
 	}
 	
 }
