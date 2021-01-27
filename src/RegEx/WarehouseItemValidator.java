7a3e55374d313f9829a364422ceb8848a14f4bd6
bhild <bryanhildebrand1@gmail.com>
2021-01-26 17:57:06 -0800

    added shell validator classes

--- src/RegEx/WarehouseItemValidator.java
+++ src/RegEx/WarehouseItemValidator.java
@@ -1,9 +1,17 @@
 package RegEx;
 
 public class WarehouseItemValidator {
-	public WarehouseItemValidator() {}//left empty -- this class will only be used as a part of the item list
+	public WarehouseItemValidator() {}
+	//left empty -- this class will only be used as a part of the item list
 	//it is going to keep separate for organizational reasons
-	
-	
+	public boolean isValidId(String id) {
+		return false;
+	}
+	public boolean isValidName(String name) {
+		return false;
+	}
+	public boolean isValidDescription(String des) {
+		return false;
+	}
 	
 }
