af0ff0d4e2357371050fad4dbe2b582199687f83
bhild <bryanhildebrand1@gmail.com>
2021-01-26 17:20:59 -0800

    added WarehouseItems and added shell vars

---
+++ src/RegEx/WarehouseItems.java
@@ -0,0 +1,27 @@
+package RegEx;
+
+public class WarehouseItems {
+	private String itemId;
+	private String itemDescription;
+	private String itemName;
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getItemDescription() {
+		return itemDescription;
+	}
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	
+	
+}
