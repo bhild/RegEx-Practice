ab94f38993b5c4a9f7b86eaeb2dde6ee469e839f
bhild <bryanhildebrand1@gmail.com>
2021-01-26 18:29:13 -0800

    fixed bugs in regex

--- src/RegEx/WarehouseItemValidator.java
+++ src/RegEx/WarehouseItemValidator.java
@@ -5,13 +5,13 @@ public class WarehouseItemValidator {
 	//left empty -- this class will only be used as a part of the item list
 	//it is going to keep separate for organizational reasons
 	public boolean isValidId(String id) {
-		return id.matches("^([0-9]|[-])$");
+		return id.matches("^([0-9]|[-])+$");
 	}
 	public boolean isValidName(String name) {
-		return name.matches("^([:alpha:]|[0-9 -]){3,}$");
+		return name.matches("^([a-zA-Z]|[0-9 -]){3,}$");
 	}
 	public boolean isValidDescription(String des) {
-		return des.matches("^(([:alpha:]|[, ]){5,}[.!;?])+$");
+		return des.matches("^(([a-zA-Z]|[, ]){5,}[.!;?])+$");
 	}
 	
 }
