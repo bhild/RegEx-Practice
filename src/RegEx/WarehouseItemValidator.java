1ec0d0ad993fd555153448a02db9a3734ee9e911
bhild <bryanhildebrand1@gmail.com>
2021-01-26 17:45:02 -0800

    added constructor

--- src/RegEx/WarehouseItemValidator.java
+++ src/RegEx/WarehouseItemValidator.java
@@ -1,6 +1,9 @@
 package RegEx;
 
 public class WarehouseItemValidator {
+	public WarehouseItemValidator() {}//left empty -- this class will only be used as a part of the item list
+	//it is going to keep separate for organizational reasons
+	
 	
 	
 }
