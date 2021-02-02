8009039296386607b6e5b349c9ede27c39f51ff6
bhild <bryanhildebrand1@gmail.com>
2021-02-01 16:25:15 -0800

    removed item validator -- moved to item list

--- src/RegEx/Driver.java
+++ src/RegEx/Driver.java
@@ -2,6 +2,5 @@ package RegEx;
 
 public class Driver {
 	public static void main(String[] args) {
-		
 	}
 }
--- src/RegEx/WarehouseItemList.java
+++ src/RegEx/WarehouseItemList.java
@@ -4,4 +4,13 @@ public class WarehouseItemList {
 	public WarehouseItemList() {
 		// TODO Auto-generated constructor stub
 	}
+	public boolean isValidId(String id) {
+		return id.matches("^([0-9]|[-])+$");
+	}
+	public boolean isValidName(String name) {
+		return name.matches("^([a-zA-Z]|[0-9 -]){3,}$");
+	}
+	public boolean isValidDescription(String des) {
+		return des.matches("^(([a-zA-Z]|[, ]){5,}[.!;?])+$");
+	}
 }
