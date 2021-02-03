a31523e302402f1037a4db2e9abf2da748b27e74
bhild <bryanhildebrand1@gmail.com>
2021-02-03 15:06:35 -0800

    added some test cases

--- src/RegEx/Driver.java
+++ src/RegEx/Driver.java
@@ -3,9 +3,11 @@ package RegEx;
 public class Driver {
 	public static void main(String[] args) {
 		WarehouseItemList list = new WarehouseItemList();
-		list.addItem("909-365", "tempName", "No description provided.");
-		list.addItem("909-365a", "tempName", "No description provided.--''()())(66.");
-		list.addItem("909-365", "te", "No description provided");
+		list.addItem("909-365", "tempName", "No description provided."); // this should work
+		list.addItem("909-365a", "tempName", "No description provided.--''()())(66."); // this has an invalid description and id
+		list.addItem("909-365", "te", "No description provided"); // the description is missing punctuation and the name is too short
+		list.addItem("someId", "potato", "Vegital.");//id is invalid
+		list.addItem("someId", "potato2369-25@", "no");//all of these are invalid
 		System.out.println(list.toString());
 	}
 }
