7c3f0524e4954747765a56219e0b8547c23aff87
bhild <bryanhildebrand1@gmail.com>
2021-02-03 14:56:53 -0800

    updated syserr

--- src/RegEx/WarehouseItemList.java
+++ src/RegEx/WarehouseItemList.java
@@ -32,26 +32,26 @@ public class WarehouseItemList {
 				if (i==0) {
 					System.err.print("name: " + name);
 					if (!isValidName(name)) {
-						if (name.length()>3) {
-							System.err.print("	contains invalid character - "+name.replaceAll(nameRegex, ""));
+						if (name.replaceAll(nameRegex,"").length()!=0) {
+							System.err.print("	contains invalid character(s) - "+name.replaceAll(nameRegex, ""));
 						}else {
-							System.err.print("	input too short or missing required characters");
+							System.err.print("	input too short or missing required character(s)");
 						}
 					}
 				}
 				else if (i==1) {
 					System.err.print("id: " + id);
 					if (!isValidId(id)) {
-						System.err.print("	contains invalid character - "+id.replaceAll(idRegex, ""));	
+						System.err.print("	contains invalid character(s) - "+id.replaceAll(idRegex, ""));	
 					}
 				}
 				else if (i==2) {
 					System.err.print("description: " + description);
 					if (!isValidDescription(description)) {
 						if (description.replaceAll(desRegex, "").length()!=0) {
-							System.err.print("	contains invalid character - "+description.replaceAll(desRegex, ""));							
+							System.err.print("	contains invalid character(s) - "+description.replaceAll(desRegex, ""));							
 						}else {
-							System.err.print("	input too short or missing required characters");
+							System.err.print("	input too short or missing required character(s)");
 						}
 					}
 				}
