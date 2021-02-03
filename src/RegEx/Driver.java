0c489d82d3f46f4089c557f963bb7c4c0710f9e8
bhild <bryanhildebrand1@gmail.com>
2021-02-02 19:53:34 -0800

    simple tests in driver

--- src/RegEx/Driver.java
+++ src/RegEx/Driver.java
@@ -2,5 +2,11 @@ package RegEx;
 
 public class Driver {
 	public static void main(String[] args) {
+		WarehouseItemList list = new WarehouseItemList();
+		list.addItem("909-365", "tempName", "No description provided.");
+		list.addItem("909-365a", "tempName", "No description provided.--''()())(66.");
+		list.addItem("909-365", "te", "No description provided");
+		System.out.println(list.toString());
 	}
 }
+	
