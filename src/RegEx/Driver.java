9283539d3e6a43e55a941e849dff783c6b59fd76
bhild <bryanhildebrand1@gmail.com>
2021-02-04 21:01:36 -0800

    changed driver

--- src/RegEx/Driver.java
+++ src/RegEx/Driver.java
@@ -1,6 +1,9 @@
 package RegEx;
 
+import java.util.Random;
+
 public class Driver {
+	private static Random r = new Random();
 	public static void main(String[] args) {
 		WarehouseItemList list = new WarehouseItemList();
 		list.addItem("909-365", "tempName", "No description provided."); // this should work
@@ -8,7 +11,10 @@ public class Driver {
 		list.addItem("909-365", "te", "No description provided"); // the description is missing punctuation and the name is too short
 		list.addItem("someId", "potato", "Vegital.");//id is invalid
 		list.addItem("someId", "potato2369-25@", "no");//all of these are invalid
+		list.addItem("98597", "Item-Name", "This is a two sentence description. This is the second sentence.");
+		list.addItem("56465444166846464646464649646868436", "validName", "klasgfn! iewhbfiBWEG. $120.00?");
 		System.out.println(list.toString());
 	}
+	
 }
 	
