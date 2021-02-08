285ead2a915049cf80603195cf6c02fdcd1d39af
bhild <42156370+bhild@users.noreply.github.com>
2021-02-07 18:52:25 -0800

    added more tests

--- src/RegEx/Driver.java
+++ src/RegEx/Driver.java
@@ -10,11 +10,13 @@ public class Driver {
 		list.addItem("909-365a", "tempName", "No description provided.--''()())(66."); // this has an invalid description and id
 		list.addItem("909-365", "te", "No description provided"); // the description is missing punctuation and the name is too short
 		list.addItem("someId", "potato", "Vegital.");//id is invalid
-		list.addItem("someId", "potato2369-25@", "no");//all of these are invalid
+		list.addItem("someId", "potato2369-25@", "no");//none of these are invalid
+		list.addItem("someId", "potato2369-25", "no");//none of these are invalid
 		list.addItem("98597", "Item-Name", "This is a two sentence description. This is the second sentence.");
-		list.addItem("56465444166846464646464649646868436", "validName", "klasgfn! iewhbfiBWEG. $120.00?");
+		list.addItem("56465444166846464646464649646868436", "validName", "klasgfn! iewhbfiBWEG. $120.00?");//allows prices
+		list.addItem("56465444166846464646464649646868436", "validName", "klasgfn! iewhbfiBWEG. $120.00 hello.");//prices should work in sentences
+		list.addItem("56465444166846464646464649646868436", "validName", "klasgfn! iewhbfiBWEG. $120.00");//prices need to be puncuated
 		System.out.println(list.toString());
 	}
 	
 }
-	
